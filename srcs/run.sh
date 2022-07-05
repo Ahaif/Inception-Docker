eabe55d5ef876ee0fed85ced1db1f156f84b5071
ahaifoul <ahaifoul@e1r4p15.1337.ma>
2022-07-05 20:12:57 +0100

    structu

--- srcs/run.sh
+++ srcs/run.sh
@@ -3,7 +3,7 @@
 mkdir -p $HOME/data/db-data
 mkdir -p $HOME/data/www-data
 mkdir -p $HOME/data/backup-data
-cp -r /var/test /home/ahaifoul/data/www-data
+
 docker-compose up --build
 docker-compose down -v
 sudo rm -rf $HOME/data
