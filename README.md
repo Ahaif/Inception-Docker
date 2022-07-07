ca918879327f82c9b3cbb4abd8cc14e7443c9851
Your Name <you@example.com>
2022-07-07 13:10:09 -0400

    mandatory

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Inception-Docker
+
+
+
+sudo docker rm -f $(sudo docker ps -aq)
+sudo docker rmi -f $(sudo docker images -q)
+docker exec -it nginx sh
--- srcs/run.sh
+++ srcs/run.sh
@@ -4,6 +4,6 @@ mkdir -p $HOME/data/db-data
 mkdir -p $HOME/data/www-data
 mkdir -p $HOME/data/backup-data
 
-docker-compose up --build
-docker-compose down -v
-sudo rm -rf $HOME/data
+docker-compose up --build -d
+# docker-compose down -v
+# sudo rm -rf $HOME/data
